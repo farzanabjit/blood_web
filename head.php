088562a3dd92b6565da77408db1411e0bf0c44e9
Farzana Rahman <131145991+farzanabjit@users.noreply.github.com>
2023-05-26 14:21:26 +0600

    Update head.php

--- head.php
+++ head.php
@@ -76,7 +76,7 @@ a.logo2{
     <a href="home.php" class="logo"<?php if($active=='home') echo "class='logo2'"; ?>>Blood Bank & Donation </a>
     <div class="header-right">
     <a href="about_us.php"  <?php if($active=='about') echo "class='act'"; ?> >About Us</a>
-    <a href="why_donate_blood.php"  <?php if($active=='why') echo "class='act'"; ?>>Why Donate Blood?</a>
+    <a href="why_donate_blood.php"  <?php if($active=='why') echo "class='act'"; ?>>Why Donate Blood</a>
       <a href="donate_blood.php"  <?php if($active=='donate') echo "class='act'"; ?>>Become A Donor</a>
       <a href="need_blood.php" <?php if($active=='need') echo "class='act'"; ?>>Need Blood</a>
       <a href="contact_us.php" <?php if($active=='contact') echo "class='act'"; ?>>Contact Us</a>
